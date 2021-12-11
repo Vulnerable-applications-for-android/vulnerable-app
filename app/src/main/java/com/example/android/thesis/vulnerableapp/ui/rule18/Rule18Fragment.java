4fb84932c9dda02128dea924ed33ae38587c5ffb
albertomolon <alberto.molon97@gmail.com>
2021-12-11 18:09:35 +0100

    Update R18

--- app/src/main/java/com/example/android/thesis/vulnerableapp/ui/rule18/Rule18Fragment.java
+++ app/src/main/java/com/example/android/thesis/vulnerableapp/ui/rule18/Rule18Fragment.java
@@ -1,7 +1,5 @@
 package com.example.android.thesis.vulnerableapp.ui.rule18;
 
-import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -34,13 +32,17 @@ public class Rule18Fragment extends Fragment {
             sendIntent.setDataAndType(Uri.parse(uri), "text/plain");
             sendIntent.putExtra("secret", "sensitive data");
             startActivity(sendIntent);
+//            startActivity(Intent.createChooser(sendIntent, "Send it using: "));
         });
 
 //        Button sendIntentActButton = root.findViewById(R.id.button_activity_rule18);
 //        sendIntentActButton.setOnClickListener(view -> {
 //            Intent sendIntent = new Intent("com.victim.ADD_CARD_ACTION");
-//            sendIntent.putExtra("secret", "ahahahahahahaahha");
-//            sendIntent.putExtra("wei", "alberto");
+//            Bundle bundle = new Bundle();
+//            bundle.putString("login", "alberto");
+//            bundle.putString("password", "very_secret");
+//            sendIntent.putExtras(bundle);
+//            sendIntent.putExtra("laughing", "ahaahahahahha");
 //            startActivity(sendIntent);
 //        });
 
@@ -48,9 +50,10 @@ public class Rule18Fragment extends Fragment {
         Button sendIntentBroButton = root.findViewById(R.id.button_broadcast_rule18);
         sendIntentBroButton.setOnClickListener(view -> {
             Intent intent = new Intent("com.victim.messenger.IN_APP_MESSAGE");
+//            intent.addCategory("example.test.category");
             intent.putExtra("from", "alberto");
             intent.putExtra("text", "Hello everyone!!");
-            context.sendBroadcast(intent, "test.permission");
+            context.sendBroadcast(intent, "test.permission");   // remember to add "test.permission" in the evilapp's manifest
         });
 
         return root;
